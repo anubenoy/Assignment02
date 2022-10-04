db368480dcd9e6a0a3e31d3121d18cb80eb7730f
anubenoy <anubenoyarackal912@gmail.com>
2022-10-04 12:52:49 -0400

    containing the completed Rectangle class

---
+++ Rectangle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment02
+{
+    internal class Rectangle
+    {
+        //variables declaration
+        int length;
+        int width;
+
+        //default constructor
+        public Rectangle{
+            this.length = 1;
+            this.width = 1;
+        }
+        //Rectangle(int lenght, int width){}
+       
+
+    }
+}
